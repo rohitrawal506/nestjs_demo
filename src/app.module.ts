be48e3c39ca73ea9426f9b1b6299a422e583a3c4
Dhruvish0146 <ddc0146@gmail.com>
2024-05-09 10:11:18 +0530

    scehma added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/authentication'),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
